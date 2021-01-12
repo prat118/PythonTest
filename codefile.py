e35e84de7d8c90caff5ce6aa70cd9404fc4a7cf6
Pratik Jagtap <adhir.3579@gmail.com>
2021-01-12 13:19:32 +0530

    Added second print statement to both the file, CodeFile & Welcome

--- codefile.py
+++ codefile.py
@@ -1,1 +1,3 @@
 print('This is Git Basics. Please continue to learn and do Enjoy !!')
+
+print('This is the change too.')
--- welcome.py
+++ welcome.py
@@ -1,1 +1,3 @@
 print('Welcome to Git.!!')
+
+print('This is the change that is being made.')
